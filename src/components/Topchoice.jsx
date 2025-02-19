482eadf5e7c4bebb826c49ffa4189bac3596ccb1
Joshua Nwajei <nwajeijoshua55@gmail.com>
2025-02-19 06:45:51 +0100

    progress

--- src/components/Topchoice.jsx
+++ src/components/Topchoice.jsx
@@ -9,14 +9,39 @@ import img5 from "../../public/images/atease.jpeg"
 
 function Topchoice() {
     const images = [ img1, img2, img3, img4 , img5]
+    // const book_names = ["God Child", "Things fall apart","Freshwater","My sister is a seral killer","No longer aat  ease"]
   return (
     <div>
         <h3>Readers Top Choice</h3>
-        <div className="books"></div>
+        <div className="books">
+      
         <div className="top-choice-img-container">
             {images.map((image, index) =>(
                 <img  className='top-choice-images' key={index} src={image} />
             ))}
+
+</div>
+            <div className='names-review'>
+            <span>God Children are little broken things <br />
+            <span id='fade'>By Arinze Ifeakandu</span>
+            <progress value={20} max={100}></progress></span>&nbsp;&nbsp;&nbsp;&nbsp;
+              <span>Things fall apart <br /><br />
+            <span id='fade'>By Chinua Achebe</span><br />
+              <progress value={50} max={100}></progress></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span>Freshwater <br /><br />
+            <span id='fade'> By Akweke Emezi</span><br />
+              <progress value={10} max={100}></progress></span>&nbsp;&nbsp;&nbsp;&nbsp;
+              <span >My sister the serial killer<br /><br />
+              <span id='fade'> By Oyikan Braithwiaith</span>   <br />      
+              <progress value={70} max={100}></progress></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <span>No longer at  ease<br /><br />
+            <span id='fade'>By Chinua Achebe</span><br />
+              <progress value={10} max={100}></progress></span>
+      
+              
+              
+
+            </div>
         </div>
     </div>
   )
